6a359304578906be86b669ef0b30d2f227a6e998
Diego Ronan Kulian <dkulian@veepee.com>
2024-12-16 19:16:54 +0100

    criando form de inclusao / alteracao

--- src/views/UserForm.js
+++ src/views/UserForm.js
@@ -1,6 +1,48 @@
-import React from 'react';
-import {Text} from 'react-native';
+import React, {useState} from 'react';
+import {Button, StyleSheet, Text, TextInput, View} from 'react-native';
 
 export default props => {
-  return <Text>UserForm</Text>;
+  const [user, setUser] = useState(
+    props.route.params ? props.route.params : {},
+  );
+
+  return (
+    <View style={style.form}>
+      <Text>Name</Text>
+      <TextInput
+        onChangeText={name => setUser({...user, name})}
+        placeholder="Informe o nome"
+        value={user.name}
+        style={style.input}
+      />
+      <Text>Email</Text>
+      <TextInput
+        onChangeText={email => setUser({...user, email})}
+        placeholder="Informe o email"
+        value={user.email}
+        style={style.input}
+      />
+      <Text>Url do Avatar</Text>
+      <TextInput
+        onChangeText={avatarUrl => setUser({...user, avatarUrl})}
+        placeholder="Informe o email"
+        value={user.avatarUrl}
+        style={style.input}
+      />
+      <Button title="Salvar" onPress={() => props.navigation.goBack()} />
+    </View>
+  );
 };
+
+const style = StyleSheet.create({
+  form: {
+    padding: 12,
+  },
+  input: {
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1,
+    marginTop: 5,
+    marginBottom: 20,
+  },
+});
