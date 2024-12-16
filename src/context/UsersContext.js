0c8aa7a78c60c484c55248a6571503d87791a3cb
Diego Ronan Kulian <dkulian@veepee.com>
2024-12-16 20:05:10 +0100

    introduzindo o dispatch / redux

--- src/context/UsersContext.js
+++ src/context/UsersContext.js
@@ -1,11 +1,21 @@
-import React, {createContext} from 'react';
+import React, {createContext, useReducer} from 'react';
 import users from '../data/users';
+
+const initialState = {users};
 
 const UsersContext = createContext({});
 
 export const UsersProvider = props => {
+  //dispatch é a função que será chamada para atualizar o estado
+  function reducer(state, action) {
+    console.log(action);
+    return state;
+  }
+
+  const [state, dispatch] = useReducer(reducer, initialState);
+
   return (
-    <UsersContext.Provider value={{state: {users}}}>
+    <UsersContext.Provider value={{state, dispatch}}>
       {props.children}
     </UsersContext.Provider>
   );
--- src/views/UserList.js
+++ src/views/UserList.js
@@ -4,14 +4,14 @@ import {ListItem, Avatar} from 'react-native-elements';
 import UsersContext from '../context/UsersContext';
 
 export default props => {
-  const {state} = useContext(UsersContext);
+  const {state, dispatch} = useContext(UsersContext);
 
   function confirmUserDeletion(user) {
     Alert.alert('Excluir Usuário', 'Deseja excluir o usuário?', [
       {
         text: 'Sim',
         onPress() {
-          console.log('delete ' + user.id);
+          dispatch({type: 'deleteUser', payload: user});
         },
       },
       {
