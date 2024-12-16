1e292ec7f1af54aad57f3d86af15dae3687697b2
Diego Ronan Kulian <dkulian@veepee.com>
2024-12-16 18:46:57 +0100

    criando lista de usuarios com listitem do react native elements

--- src/views/UserList.js
+++ src/views/UserList.js
@@ -1,6 +1,31 @@
 import React from 'react';
-import {Text} from 'react-native';
+import {FlatList, View} from 'react-native';
+import users from '../data/users';
+import {ListItem, Avatar} from 'react-native-elements';
 
 export default props => {
-  return <Text>UserList</Text>;
+  function getUserItem({item: user}) {
+    return (
+      <ListItem
+        key={user.id}
+        bottomDivider
+        onPress={() => props.navigation.navigate('UserForm')}>
+        <Avatar rounded source={{uri: user.avatarUrl}} />
+        <ListItem.Content>
+          <ListItem.Title>{user.name}</ListItem.Title>
+          <ListItem.Subtitle>{user.email}</ListItem.Subtitle>
+        </ListItem.Content>
+      </ListItem>
+    );
+  }
+
+  return (
+    <View>
+      <FlatList
+        keyExtractor={user => user.id.toString()}
+        data={users}
+        renderItem={getUserItem}
+      />
+    </View>
+  );
 };
