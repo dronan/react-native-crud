87c3bda83a092205370e3c6594698b45dde76c58
Diego Ronan Kulian <dkulian@veepee.com>
2024-12-16 18:58:31 +0100

    criando botoes de acao e popup de confirmacao

--- src/views/UserList.js
+++ src/views/UserList.js
@@ -1,20 +1,44 @@
 import React from 'react';
-import {FlatList, View} from 'react-native';
+import {FlatList, View, Alert} from 'react-native';
 import users from '../data/users';
 import {ListItem, Avatar} from 'react-native-elements';
 
 export default props => {
+  function confirmUserDeletion(user) {
+    Alert.alert('Excluir Usuário', 'Deseja excluir o usuário?', [
+      {
+        text: 'Sim',
+        onPress() {
+          console.log('delete ' + user.id);
+        },
+      },
+      {
+        text: 'Não',
+      },
+    ]);
+  }
+
   function getUserItem({item: user}) {
     return (
       <ListItem
         key={user.id}
         bottomDivider
-        onPress={() => props.navigation.navigate('UserForm')}>
+        onPress={() => props.navigation.navigate('UserForm', user)}>
         <Avatar rounded source={{uri: user.avatarUrl}} />
         <ListItem.Content>
           <ListItem.Title>{user.name}</ListItem.Title>
           <ListItem.Subtitle>{user.email}</ListItem.Subtitle>
         </ListItem.Content>
+        <ListItem.Chevron
+          onPress={() => navigation.navigate('userForm', user)}
+          iconProps={{name: 'edit'}}
+          iconStyle={{fontSize: 25, color: 'orange'}}
+        />
+        <ListItem.Chevron
+          onPress={() => confirmUserDeletion(user)}
+          iconProps={{name: 'delete'}}
+          iconStyle={{fontSize: 25, color: 'red'}}
+        />
       </ListItem>
     );
   }
