c0276f23d18e6acca8b3d6576266b7a4ec984301
Diego Ronan Kulian <dkulian@veepee.com>
2024-12-16 17:58:23 +0100

    corrigindo icone e adicionando navegacao inicial basica

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
-
+import {Button, Icon} from 'react-native-elements';
 import UserList from './views/UserList';
 import UserForm from './views/UserForm';
 
@@ -10,10 +10,41 @@ const Stack = createStackNavigator();
 export default props => {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="UserList">
-        <Stack.Screen name="UserList" component={UserList} />
-        <Stack.Screen name="UserForm" component={UserForm} />
+      <Stack.Navigator
+        initialRouteName="UserList"
+        screenOptions={screenOptions}>
+        <Stack.Screen
+          name="UserList"
+          component={UserList}
+          options={({navigation}) => {
+            return {
+              title: 'Lista de Usuarios',
+              headerRight: () => (
+                <Button
+                  onPress={() => navigation.navigate('UserForm')}
+                  type="clear"
+                  icon={<Icon name="add" size={25} color="white" />}
+                />
+              ),
+            };
+          }}
+        />
+        <Stack.Screen
+          name="UserForm"
+          component={UserForm}
+          options={{title: 'Formulario de Usuarios'}}
+        />
       </Stack.Navigator>
     </NavigationContainer>
   );
+};
+
+const screenOptions = {
+  headerStyle: {
+    backgroundColor: '#c56363',
+  },
+  headerTintColor: '#fff',
+  headerTitleStyle: {
+    fontWeight: 'bold',
+  },
 };
